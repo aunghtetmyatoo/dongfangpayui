042ff9b579ce7278303e49d77e8d972eabd88d34
aunghtetmyatoo <aunghtetmyatoo888@gmail.com>
2022-06-03 08:27:21 +0630

    modified & added files

---
+++ assets/js/faq.js
@@ -0,0 +1,235 @@
+$(document).ready(function () {
+  $('.q1').click(function () {
+
+    $('.faq_title').text("What is 'DONGFANG PAY'");
+    const element = document.querySelector('.faq_par');
+    element.classList.add('animate__animated', 'animate__bounceInLeft');
+    element.addEventListener('animationend', () => {
+      element.classList.remove(
+        'animate__animated',
+        'animate__bounceInLeft'
+      );
+    })
+
+    $('.faq_par').text(
+      "'DONGFANG PAY' is a mobile wallet application which is implemented for financial services provided by DONGFANG IT SERVICES COMPANY LIMITED. It can be downloaded on all phone devices for free. Functions such as phone bill top up, electricity and water bill payment, purchasing your preferable products, buying trips tickets, transaction processes, cash in and withdraw processes can be easily performed by DONGFANG PAY App. You do not have to worry about the opening and closing hours of banks since this app does not need you to have a bank account. It will offer you a safer life as you do not have to bring your money and go outside to make cash payment."
+    );
+  });
+
+  $('.q2').click(function () {
+    const element = document.querySelector('.faq_par');
+    element.classList.add('animate__animated', 'animate__bounceInLeft');
+    element.addEventListener('animationend', () => {
+      element.classList.remove(
+        'animate__animated',
+        'animate__bounceInLeft'
+      );
+    })
+    $('.faq_title').text(
+      "How to withdraw money from 'DONGFANG PAY' Account"
+    );
+    $('.faq_par').text(
+      "You can withdraw money from your 'DONGFANG PAY' Account, through Agent Counters Accounts assigned by DONGFANG IT SERVICES COMPANY LIMITED. You will have to go to Agent Counter Account and transfer the amount you would like to withdraw to his/her account. The action can be done by going to “Transfer” under Home Screen of the app. Mostly, transaction can be done by scanning QR codes provided by Counter Accounts. If the notification of successful payment transfer is received, Agent will provide the receipt together with the cash"
+    );
+  });
+
+  $('.q3').click(function () {
+    const element = document.querySelector('.faq_par');
+    element.classList.add('animate__animated', 'animate__bounceInLeft');
+    element.addEventListener('animationend', () => {
+      element.classList.remove(
+        'animate__animated',
+        'animate__bounceInLeft'
+      );
+    })
+    $('.faq_title').text(
+      "How to deposit money to 'DONGFANG PAY' Account?"
+    );
+    $('.faq_par').text(
+      "You can deposit money to your 'DONGFANG PAY' Account, through Agent Counters Accounts assigned by DONGFANG IT SERVICES COMPANY LIMITED. You will have to go to Agent Counter Account and give the cash of the amount you would like to deposit to your account. The Agent will deposit the amount to your DONGFANG PAY account after cash check process is completed."
+    );
+  });
+
+  $('.q4').click(function () {
+    const element = document.querySelector('.faq_par');
+    element.classList.add('animate__animated', 'animate__bounceInLeft');
+    element.addEventListener('animationend', () => {
+      element.classList.remove(
+        'animate__animated',
+        'animate__bounceInLeft'
+      );
+    })
+    $('.faq_title').text(
+      "How to change passcode in 'DONGFANG PAY' app? "
+    );
+    $('.faq_par').text(
+      "You can change the passcode in 'DONGFANG PAY' App by going to ‘Change Passcode’ under Profile. The old passcode will have to be entered correctly first and you can set your new passcode if the old one is correctly entered. "
+    );
+  });
+
+  $('.q5').click(function () {
+    const element = document.querySelector('.faq_par');
+    element.classList.add('animate__animated', 'animate__bounceInLeft');
+    element.addEventListener('animationend', () => {
+      element.classList.remove(
+        'animate__animated',
+        'animate__bounceInLeft'
+      );
+    })
+    $('.faq_title').text('What does DongFang Pay Partner mean? ');
+    $('.faq_par').text(
+      'DongFang Pay Partner means the merchants who have a legal contract with DONGFANG IT SERVICES COMPANY LIMITED, the owner of the DongFang Pay application. Merchants have a separate contract to pay for the goods with the DongFang Pay application up to the specified amount. Customers can easily pay by scanning the Partner QR Code at these partner stores. '
+    );
+  });
+
+  $('.q6').click(function () {
+    const element = document.querySelector('.faq_par');
+    element.classList.add('animate__animated', 'animate__bounceInLeft');
+    element.addEventListener('animationend', () => {
+      element.classList.remove(
+        'animate__animated',
+        'animate__bounceInLeft'
+      );
+    })
+    $('.faq_title').text(
+      'What is a coupon? How to use this coupon code? '
+    );
+    $('.faq_par').text(
+      'Coupon is a discount code that can be used to make a payment after purchasing products at DongFang Pay Partner stores. There are two types of discount coupons: Fixed Amount coupons and Percentage coupons with a percentage discount. You can find coupons from the Profile screen with the coupon code you received. If so, you can copy the coupon code by clicking the Copy Coupon, scan the Partner QR Code at the Partner Store and enter this coupon code to make the payment. '
+    );
+  });
+
+  $('.q7').click(function () {
+    const element = document.querySelector('.faq_par');
+    element.classList.add('animate__animated', 'animate__bounceInLeft');
+    element.addEventListener('animationend', () => {
+      element.classList.remove(
+        'animate__animated',
+        'animate__bounceInLeft'
+      );
+    })
+    $('.faq_title').text(
+      "How to use ‘Fingerprint’ system on 'DONGFANG PAY' app? "
+    );
+    $('.faq_par').text(
+      'In order to use the fingerprint system on your phone, you will need to set up your fingerprint under phone setting first. Then, please go to ‘Biometric Setting’ under DONGFANG PAY App Profile. If Fingerprint/ Face ID is on, the fingerprint system can be used for your DONGFANG PAY App. '
+    );
+  });
+
+  $('.q8').click(function () {
+    const element = document.querySelector('.faq_par');
+    element.classList.add('animate__animated', 'animate__bounceInLeft');
+    element.addEventListener('animationend', () => {
+      element.classList.remove(
+        'animate__animated',
+        'animate__bounceInLeft'
+      );
+    })
+    $('.faq_title').text(
+      "How to transfer money on 'DONGFANG PAY' app?  "
+    );
+    $('.faq_par').text(
+      'Press ‘Transfer’ under Home Screen. Then, please enter the phone number or choose the registered phone for DONGFANG PAY Account or choose the number you would like to transfer from the Recent Transaction List, and enter the complete and correct phone number (for e.g, +959448047672). If the entered phone number is correctly registered as DONGFANG PAY Account, you will reach to the screen where the transfer amount has to be entered. For the final step, the passcode have to be entered correctly. In other way, the recipient can provide QR code and transfer process can be done by scanning it. '
+    );
+  });
+
+  $('.q9').click(function () {
+    const element = document.querySelector('.faq_par');
+    element.classList.add('animate__animated', 'animate__bounceInLeft');
+    element.addEventListener('animationend', () => {
+      element.classList.remove(
+        'animate__animated',
+        'animate__bounceInLeft'
+      );
+    })
+    $('.faq_title').text('Why is DONGFANG PAY Account suspended? ');
+    $('.faq_par').text(
+      'If you have wrongly entered your OTP for three times, the account will be suspended temporarily for user safety. If you would like to reactivate your account, please kindly contact to DONGFANG PAY Help Center. '
+    );
+  });
+
+  $('.q10').click(function () {
+    const element = document.querySelector('.faq_par');
+    element.classList.add('animate__animated', 'animate__bounceInLeft');
+    element.addEventListener('animationend', () => {
+      element.classList.remove(
+        'animate__animated',
+        'animate__bounceInLeft'
+      );
+    })
+    $('.faq_title').text(
+      'What process should be done to make DONGFANG PAY Account secure? '
+    );
+    $('.faq_par').text(
+      'One Time Passcode (OTP) should not be shared to anyone. Please avoid using the app without changing the passcode and it should be changed once in three months or six months. If necessary, you can log out from the app after using. Please be informed that DONGFANG PAY Help Center will never ask for OTP from DONGFANG PAY Users for any reasons and therefore, please be careful not to share the OTP with anyone else for any reason. '
+    );
+  });
+
+  $('.q11').click(function () {
+    const element = document.querySelector('.faq_par');
+    element.classList.add('animate__animated', 'animate__bounceInLeft');
+    element.addEventListener('animationend', () => {
+      element.classList.remove(
+        'animate__animated',
+        'animate__bounceInLeft'
+      );
+    })
+    $('.faq_title').text(
+      'How to do if DONGFANG PAY Account registered SIMcard was lost?'
+    );
+    $('.faq_par').text(
+      'If DONGFANG PAY Account registered SIMcard was lost or the phone where DONGFANG PAY Account was logged in, was lost, please contact DONGFANG PAY Help Center as soon as possible and Center will advise you for the necessary points to take action'
+    );
+  });
+
+  $('.q12').click(function () {
+    const element = document.querySelector('.faq_par');
+    element.classList.add('animate__animated', 'animate__bounceInLeft');
+    element.addEventListener('animationend', () => {
+      element.classList.remove(
+        'animate__animated',
+        'animate__bounceInLeft'
+      );
+    })
+    $('.faq_title').text(
+      'What is the maximum amount that the transaction can be done in Level 1? '
+    );
+    $('.faq_par').text(
+      'You can check the limitations in details by going to ‘Limit & Fees’ under Profile. '
+    );
+  });
+
+  $('.q13').click(function () {
+    const element = document.querySelector('.faq_par');
+    element.classList.add('animate__animated', 'animate__bounceInLeft');
+    element.addEventListener('animationend', () => {
+      element.classList.remove(
+        'animate__animated',
+        'animate__bounceInLeft'
+      );
+    })
+    $('.faq_title').text(
+      'How to give feedback/advice related with DONGFANG PAY Software?'
+    );
+    $('.faq_par').text(
+      'You can enter “Help & Feedback” under Profile and report the issues you are facing either by words or photos. The same action can be done if you would like to give advice for software'
+    );
+  });
+
+  $('.q14').click(function () {
+    const element = document.querySelector('.faq_par');
+    element.classList.add('animate__animated', 'animate__bounceInLeft');
+    element.addEventListener('animationend', () => {
+      element.classList.remove(
+        'animate__animated',
+        'animate__bounceInLeft'
+      );
+    })
+    $('.faq_title').text(
+      "What should I do if I forgot my passcode to log in 'DONGFANG PAY' app? "
+    );
+    $('.faq_par').text(
+      "Please press 'Forgot Passcode' and one time 6 digits passcodes (OTP) will be received. If you have successfully entered your OTP, the security questions which you have filled up while registering for the account, will have to be answered correctly. Then, the new password can be set up and you can log in to your account using the new passcode. "
+    );
+  });
+});
